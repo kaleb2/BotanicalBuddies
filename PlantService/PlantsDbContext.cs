c1eda272a9d0d4970e0d5ddb0ed1a66a6fc28558
Kaleb Striplin <kaleb.striplin@thermofisher.com>
2022-05-23 10:41:19 -0700

    Added a PlantService
    Added a .NET microservice called PlantService for creating plants, and getting known plants.
    It uses the MinimalAPI template and uses dependency injection to create a database connection.

---
+++ PlantService/PlantsDbContext.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
+public class PlantsDbContext : DbContext {
+
+        public PlantsDbContext(DbContextOptions<PlantsDbContext> options) 
+            : base(options) { }
+
+        public DbSet<Plant> Plants => Set<Plant>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            new PlantEntityTypeConfiguration().Configure(modelBuilder.Entity<Plant>());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseNpgsql(GetConnectionString());
+        }
+            
+        public static string GetConnectionString()
+        {
+            var server = "localhost";
+            var port = "5432";
+            var name = "botanicaldb";
+            var user = "my_botanical_buddy";
+            var password = "b0tBudz41851322";
+
+            return $"Host={server};Database={name};Port={port};Username={user};Password={password}";
+        }
+    }
