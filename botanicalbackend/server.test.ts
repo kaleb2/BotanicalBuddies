49eae42546f308c17d992628a315cef84cf218d7
George Olson <golson@pdx.edu>
2022-05-23 13:34:08 -0700

    Tests are now working

--- botanicalbackend/server.test.ts
+++ botanicalbackend/server.test.ts
@@ -1,19 +1,45 @@
 import { assert } from "console";
 import request from "supertest";
 import express from "express";
+import "dotenv/config";
 
 import setupRoutes from "./src/setupRoutes";
 
 const app = express();
 setupRoutes(app);
 
-describe("Static Routes", () => {
-  it(`responds with "GET to /welcome" text and 200 status code`, async () => {
+describe("Get Routes", () => {
+  it(`responds with "welcome" text and 200 status code`, async () => {
     let res = await request(app)
       .get("/api/v1/welcome")
       .expect(200);
 
     expect(res.text).toContain('Welcome');
+  });
+});
+
+describe("Post Routes", () => {
+  const newUser = {
+    email: "test" + Math.random() + "@jest.com",
+    password: "password123"
+  }
+
+  it(`responds with 200 status code when creating new user`, async () => {
+
+    let res = await request(app)
+      .post("/api/v1/users")
+      .send(newUser)
+      .expect(200);
+
+    expect(res.text).toContain('Signup successful');
+  });
+
+  it(`responds with 400 status code when user already exists`, async () => {
+
+    let res = await request(app)
+      .post("/api/v1/users")
+      .send(newUser)
+      .expect(400);
   });
 });
 
