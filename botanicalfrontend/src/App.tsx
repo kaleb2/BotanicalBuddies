0d07c79e773c474edc31d0e9dbdbde8525e2f5d5
George Olson <golson@pdx.edu>
2022-05-30 11:25:38 -0700

    made sure to fix some things in files after merge

--- botanicalfrontend/src/App.tsx
+++ botanicalfrontend/src/App.tsx
@@ -6,7 +6,7 @@ import { Login } from './components/Login';
 import { UserProfile } from './components/UserProfile';
 import { CreatePlant } from './components/CreatePlant';
 import { UserPlant } from './components/UserPlant';
-import { CreateJournalEntry } from './components/CreateJournalEntry';
+import { CreateJournalEntry } from './components/Journal/CreateJournalEntry';
 import { AllJournals } from './components/Journal/AllJournals';
 import { JournalEntry } from './components/Journal/JournalEntry';
 import { Journal } from './components/Journal/Journal';
