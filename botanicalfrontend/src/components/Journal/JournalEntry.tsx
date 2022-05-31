86703e32ad033fb15ed0b178b835a33767bc6781
George Olson <golson@pdx.edu>
2022-05-31 16:36:23 -0700

    include plant info on journal entry page

--- botanicalfrontend/src/components/Journal/JournalEntry.tsx
+++ botanicalfrontend/src/components/Journal/JournalEntry.tsx
@@ -6,6 +6,7 @@ import getInitialState from "../../initialState";
 import { JournalEntriesList } from "./JournalEntriesList";
 import { getAllJournalEntries, getJournalEntry } from "../../services/JournalService";
 import { PlantPage } from "../Plant/PlantPage";
+import { getPlant } from "../../services/PlantService";
 
 const initialEntryState = {
     entryId: 0,
@@ -18,12 +19,24 @@ const initialEntryState = {
     dateCreated: new Date(),
   };
 
+  const initialPlantState = {
+    plantId: 0,
+    name: "",
+    userId: 0,
+    image: "",
+    species: "",
+    dateAcquired: new Date(),
+    lastRepot: new Date(),
+    lastFertilize: new Date()
+  };
+
 export function JournalEntry() {
 
     const {id, journalId} = useParams();
 
     console.log(id);
     let [journalEntry, setJournalEntry] = useState(initialEntryState);
+    let [plant, setPlant] = useState(initialPlantState);
 
     useEffect(() => {
       console.log("journalID = " + journalId);
@@ -34,6 +47,11 @@ export function JournalEntry() {
           console.log(item);
           if (mounted) {
               setJournalEntry(item);
+              getPlant(id).then(item => {
+                console.log(item);
+                setPlant(item);
+                console.log(plant);
+            });
           }
       });
       return;
@@ -41,11 +59,21 @@ export function JournalEntry() {
 
     return (
         <div className="journal container">
-            <h1>{journalEntry.entryTitle}</h1>
             <div className="row">
-                <div className="col-md-8">
+                <div className="col-md-9">
+                    <h1>{journalEntry.entryTitle}</h1>
                     <br />
+                    <p>Written by user ID {journalEntry.userId} on {journalEntry.dateCreated.toString()}</p>
                     {journalEntry.content}
+                </div>
+                <div className="col-md-3">
+                    <h3>About this plant:</h3>
+                    <img src={plant.image} alt={plant.name} className="img-fluid float-left"/>
+                    <h4 className="w-64 ml-2">{plant.name}</h4>
+                    <p>Species: {plant.species}</p>
+                    <p>Date acquired: {plant.dateAcquired.toString()}</p>
+                    <p>Last time repot: {plant.lastRepot.toString()}</p>
+                    <p>Last time fertilized: {plant.lastFertilize.toString()}</p>
                 </div>
             </div>
         </div>
